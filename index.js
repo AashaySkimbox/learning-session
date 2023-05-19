90131d3704ca30fc72efcb3bd2485eee15950510
valaykamol <vlykamol@outlook.com>
2023-05-19 12:12:07 +0530

    counting string

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hey from valay again');
+const s = 'valay'
+
+const countChar = (ss) => {
+  let obj = {}
+
+  let temp = ss.split('')
+
+  temp.map((s) => {
+    if(obj[s]){
+      obj[s] += 1
+    }else{
+      obj[s] = 1
+    }
+  })
+
+  console.log('ans --- ', ss, obj);
+}
+
+countChar(s)
